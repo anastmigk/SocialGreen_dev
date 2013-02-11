4df0fea1d402310fa4c7d262e035da6cd2c947e4
konos <k0nos@hotmail.com>
2013-02-11 22:03:14 +0200

    Game page + tooltips

--- application/views/helpers/LoggedInAs.php
+++ application/views/helpers/LoggedInAs.php
@@ -20,9 +20,9 @@ class Zend_View_Helper_LoggedInAs extends Zend_View_Helper_Abstract
             $edit ='<li class="has-dropdown">
             			<a href="'.$editUrl.'">'.$username.'</a>
 	            		<ul class="dropdown">
-		           			<li><a href='.$this->view->url(array('controller'=>'account','action'=>'profile', 'usr'=>$username)).'>More info</a></li>
-           					<li><a href='.$this->view->url(array('controller'=>'account','action'=>'edit')).'>Edit</a></li>
-           					<li><a href="'.$logoutUrl.'">Logout</a></li>
+		           			<li><a href='.$this->view->url(array('controller'=>'account','action'=>'profile', 'usr'=>$username)).' title="Dig in">More info</a></li>
+           					<li><a href='.$this->view->url(array('controller'=>'account','action'=>'edit')).' title="Change your profile">Edit</a></li>
+           					<li><a href="'.$logoutUrl.'" title="Bye">Logout</a></li>
             			</ul>
            			</li>';
             
@@ -31,11 +31,11 @@ class Zend_View_Helper_LoggedInAs extends Zend_View_Helper_Abstract
             //return '<li><a href="'.$logoutUrl.'">Logout</a></li><li><a href="'.$editUrl.'">Profile</a></li>';
             //return $edit;
       
-            return '<li><a href='. $this->view->url(array('controller'=>'greenladder'), null, TRUE).'>Game</a></li>
-    				<li><a href='. $this->view->url(array('controller'=>'account'), null, TRUE).'>Users</a></li>
-            		<li><a href='.$this->view->url(array('controller'=>'account','action'=>'profile', 'usr'=>$username)).'>More info</a></li>
-           			<li><a href='.$this->view->url(array('controller'=>'account','action'=>'edit')).'>Edit</a></li>
-           			<li><a href="'.$logoutUrl.'">Logout</a></li>
+            return '<li><a href='. $this->view->url(array('controller'=>'greenladder'), null, TRUE).' title="Check out the game">Game</a></li>
+    				<li><a href='. $this->view->url(array('controller'=>'account'), null, TRUE).' title="We have lots of users">Users</a></li>
+            		<li><a href='.$this->view->url(array('controller'=>'account','action'=>'profile', 'usr'=>$username)).' title="Find out more">More info</a></li>
+           			<li><a href='.$this->view->url(array('controller'=>'account','action'=>'edit')).' title="Change your info">Edit</a></li>
+           			<li><a href="'.$logoutUrl.'" title="Bye">Logout</a></li>
 					  </li>';
         } 
 
@@ -54,10 +54,10 @@ class Zend_View_Helper_LoggedInAs extends Zend_View_Helper_Abstract
         }
 */
         $loginUrl = $this->view->url(array('controller'=>'auth', 'action'=>'index'));
-        $registerUrl = "<li><a id='buttonModalRegister' href=".$this->view->url(array('controller'=>'account', 'action'=>'register'), null, TRUE).">Register</a></li>";
+        $registerUrl = "<li><a id='buttonModalRegister' href=".$this->view->url(array('controller'=>'account', 'action'=>'register'), null, TRUE)." title='Be a member!'>Register</a></li>";
         //$registerUrl = "<li><a id='buttonModalRegister' href='#'>Register</a></li>";
         
-        return '<li><a href="'.$loginUrl.'">Login</a></li>'.$registerUrl;
+        return '<li><a href="'.$loginUrl.'" title="Get inside Social Green!">Login</a></li>'.$registerUrl;
         //return '<li><a id="buttonModalLogin" href="#">Login</a></li>'.$registerUrl;
 
     }
