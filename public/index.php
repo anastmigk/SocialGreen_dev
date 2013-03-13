4e858ce112936a8c7bd6726fe43a2767ffd550c4
konos <k0nos@hotmail.com>
2013-03-13 23:47:57 +0200

    lets hope this works (missing files)

--- public/index.php
+++ public/index.php
@@ -13,14 +13,31 @@ set_include_path(implode(PATH_SEPARATOR, array(
     realpath(APPLICATION_PATH . '/../library'),
     get_include_path(),
 )));
+define('APPLICATION_LIBRARY','../library');
+set_include_path(implode(PATH_SEPARATOR, array(
+APPLICATION_LIBRARY, get_include_path(),
+)));
+
+//exit(get_include_path());
+//if ( file_exists(APPLICATION_LIBRARY . '/Zend/Loader/Autoloader.php') ) {
+	//require_once('Zend/Filter/Word/CamelCaseToDash.php');
+	//$zf_filter = new Zend_Filter_Word_CamelCaseToDash();
+	//echo $zf_filter->filter('HelloWorld');
+	// echos Hello-World
+	
+	require_once 'Zend/Loader/Autoloader.php';
+	$autoloader = Zend_Loader_Autoloader::getInstance();
+	//se ->setFallbackAutoloader(true);
+//} else {
+	//exit('The Zend library folder is missing!');
+//}
 
 /** Zend_Application */
-require_once 'Zend/Application.php';
+//require_once 'Zend/Application.php';
 
 // Create application, bootstrap, and run
 $application = new Zend_Application(
     APPLICATION_ENV,
     APPLICATION_PATH . '/configs/application.ini'
 );
-$application->bootstrap()
-            ->run();
+$application->bootstrap()->run();
