17ccd193d0b487881ac88f2bb3caf0c83feda005
lapswr <lapswr@gmail.com>
2013-10-11 13:02:21 +0300

    Site Version 2.1.0

--- application/controllers/AccountController.php
+++ application/controllers/AccountController.php
@@ -245,8 +245,7 @@ class AccountController extends Zend_Controller_Action
         $paginator2->setCurrentPageNumber($page2);
         
         //$this->view->paginator=$paginator;
-        $this->view->results2 = $paginator2;
-        
+        $this->view->results2 = $paginator2; 
         
     }
 
